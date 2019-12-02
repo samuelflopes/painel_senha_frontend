e588b2e47f3c6d62dceffea416ccc77b7f26edd5
samuelflopes <52094778+samuelflopes@users.noreply.github.com>
2019-12-02 15:02:38 -0300

    Subindo arquivos
    Update nos arquivos do projeto

---
+++ main.js
@@ -0,0 +1,68 @@
+//main
+// Pesquisar cada função do const
+const {app,BrowserWindow,ipcMain} = require('electron');
+
+// JANELA PRINCIPAL
+//App inicializa a janela 
+app.on('ready', () => { // (APP, GERENCIA EVENTOS)
+    // Print Aplicação
+    console.log('Aplicação inicial')
+    let mainWindow = new BrowserWindow({
+        width:600, //fullscreen  browser electron
+        height:400,
+        //x:50, // Separação de 50px no eixo X
+        //y:50, // Separação de 50 px no eixo Y
+        //resizable:false // Perguntar se a janela pode ser redimencionada ou não;
+        show: false,
+        webPreferences:{
+            nodeIntegration:true
+        }
+    });
+    // CARREGANDO A URL DA PAGINA COM O CONTEUDO PRINCIPAL
+    //console.log(`${__dirname}`); 
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+
+    // ready-to-show, é executado quando o html estiver pronto para ser exibido.
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show();
+    });
+
+    //tipoSenha.loadURL(`file://${__dirname}/app/tipoSenha.html`);
+
+});
+
+app.on('window-all-close',() =>{
+    app.quit();
+});
+
+
+
+
+
+
+
+
+
+/*
+// JANELA DE TIPO DE SENHA
+let tipoSenha = null;
+ipcMain.on('iniciar',() => {
+
+    if (tipoSenha == null){
+        let tipoSenha = new BrowserWindow({
+            width:600,
+            height:400,
+            alwaysOnTop: true,
+            frame: false // tira a barra de opções superior
+    });
+    tipoSenha.on('closed', ()=>{
+        tipoSenha = null;
+    })
+}
+tipoSenha.loadURL(`file://${__dirname}/app/tipoSenha.html`);
+});
+
+ipcMain.on('fechar-janela-sobre',() => {
+    tipoSenha.close();
+});
+*/
